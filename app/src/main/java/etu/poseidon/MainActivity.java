34efb75c9a43e1a3082888d7c39d462c3b8111b9
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-04-11 16:05:58 +0200

    task: ajout de OpenStreetMap
    Ajout de la map OpenStreetMap avec les commentaires pour bien comprendre le code #1

--- app/src/main/java/etu/poseidon/MainActivity.java
+++ app/src/main/java/etu/poseidon/MainActivity.java
@@ -1,14 +1,61 @@
 package etu.poseidon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView map;
+    private IMapController gestionnaireMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Les configurations par défaut de l'appareil
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_main);
+
+        // On récupère le layout de la map dans le activity_main.xml
+        map = findViewById(R.id.carte);
+
+        // Récupère la map en ligne
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        // Active les zooms
+        map.setBuiltInZoomControls(true);
+
+        // Crée le point de départ de classe GeoPoint qu'on pourra personnalisé
+        // par la suite (avantage du geoPoint) et doit être utilisé par le gestionnaire de la map
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+
+        // On récupère le gestionnaire de la map pour poser des points, centrer la position, le zoom, etc.
+        gestionnaireMap = map.getController();
+
+        // Pour centrer la map sur la coordonnées stating point (43.65020, 7.00517)
+        // au démarrage de l'application
+        gestionnaireMap.setCenter(startPoint);
+
+        // Pour mettre le niveau de zoom à 20.0
+        gestionnaireMap.setZoom(20.0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
     }
 }
