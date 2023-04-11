0921df421bc9ac2421f227de567656a064ffbe36
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2023-04-11 17:52:18 +0200

    LOC FOUND

--- app/src/main/java/etu/poseidon/MainActivity.java
+++ app/src/main/java/etu/poseidon/MainActivity.java
@@ -3,12 +3,26 @@ package etu.poseidon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_lat, tv_lon,tv_altitude,tv_accuracy,tv_speed,tv_sensor,tv_updates,tv_address;
+    Switch sw_locationupdates,sw_gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+        
     }
 }
