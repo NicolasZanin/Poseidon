e73285bce3bfe0d73fbff90faf82fb785054f799
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2023-04-25 20:49:10 +0200

    fix: fix merge

--- app/src/main/java/etu/poseidon/MainActivity.java
+++ app/src/main/java/etu/poseidon/MainActivity.java
@@ -52,13 +52,8 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsDisplay;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.Overlay;
-
-import java.io.File;
-import java.util.List;
 
 import java.util.Locale;
-import java.util.Objects;
 
 import etu.poseidon.models.Poi;
 import etu.poseidon.webservices.pois.PoiApiClient;
@@ -70,13 +65,6 @@ public class MainActivity extends AppCompatActivity implements WeatherConditionU
     private MapView map;
     private IMapController gestionnaireMap;
 
-    private void ajouterPoint(double x, double y) {
-        List<Overlay> listPointMap = map.getOverlays();
-        GeoPoint pointAAdd = new GeoPoint(x, y);
-        Marker marker = new Marker(map);
-        marker.setPosition(pointAAdd);
-        listPointMap.add(marker);
-    }
     private Fragment openedFragment;
 
     private final String TAG = "JULIAN " + getClass().getSimpleName();
@@ -104,13 +92,39 @@ public class MainActivity extends AppCompatActivity implements WeatherConditionU
         Configuration.getInstance().load(getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_main);
+        // On récupère le layout de la map dans le activity_main.xml
         map = findViewById(R.id.carte);
+
+        // Récupère la map en ligne
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+
+        // Active les zooms
+        map.getZoomController().activate();
+        CustomZoomButtonsDisplay custom = map.getZoomController().getDisplay();
+        custom.setPositions(false,
+                CustomZoomButtonsDisplay.HorizontalPosition.RIGHT,
+                CustomZoomButtonsDisplay.VerticalPosition.CENTER);
+
+        // Crée le point de départ de classe GeoPoint qu'on pourra personnalisé
+        // par la suite (avantage du geoPoint) et doit être utilisé par le gestionnaire de la map
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+
+        // On récupère le gestionnaire de la map pour poser des points, centrer la position, le zoom, etc.
+        gestionnaireMap = map.getController();
+
+        // Pour centrer la map sur la coordonnées stating point (43.65020, 7.00517)
+        // au démarrage de l'application
+        gestionnaireMap.setCenter(startPoint);
+
+        // Pour mettre le niveau de zoom à 20.0
+        gestionnaireMap.setZoom(20.0);
         locationRequest = new LocationRequest();
         locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
         locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        startLocationUpdates();
+        /*startLocationUpdates();*/
         locationCallBack = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
@@ -191,34 +205,6 @@ public class MainActivity extends AppCompatActivity implements WeatherConditionU
     }
 
     private void updateUIValues(Location location) {
-
-        // On récupère le layout de la map dans le activity_main.xml
-        map = findViewById(R.id.carte);
-
-        // Récupère la map en ligne
-        map.setTileSource(TileSourceFactory.MAPNIK);
-
-
-        // Active les zooms
-        map.getZoomController().activate();
-        CustomZoomButtonsDisplay custom = map.getZoomController().getDisplay();
-        custom.setPositions(false,
-                CustomZoomButtonsDisplay.HorizontalPosition.RIGHT,
-                CustomZoomButtonsDisplay.VerticalPosition.CENTER);
-
-        // Crée le point de départ de classe GeoPoint qu'on pourra personnalisé
-        // par la suite (avantage du geoPoint) et doit être utilisé par le gestionnaire de la map
-        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
-
-        // On récupère le gestionnaire de la map pour poser des points, centrer la position, le zoom, etc.
-        gestionnaireMap = map.getController();
-
-        // Pour centrer la map sur la coordonnées stating point (43.65020, 7.00517)
-        // au démarrage de l'application
-        gestionnaireMap.setCenter(startPoint);
-
-        // Pour mettre le niveau de zoom à 20.0
-        gestionnaireMap.setZoom(20.0);
         // Button to open weather condition creator
         Button buttonWeatherConditionCreator = findViewById(R.id.button_weather_condition_creator);
         buttonWeatherConditionCreator.setOnClickListener(v -> {
