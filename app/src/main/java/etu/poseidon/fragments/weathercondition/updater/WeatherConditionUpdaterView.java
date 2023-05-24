751bc2d76cdd81141a980259882e75646d14547f
Alexis Malosse <alexis.malosse@etu.unice.fr>
2023-05-24 16:04:17 +0200

    refactor: separate weather conditon updater fragment setup into differents functions #22

--- app/src/main/java/etu/poseidon/fragments/weathercondition/updater/WeatherConditionUpdaterView.java
+++ app/src/main/java/etu/poseidon/fragments/weathercondition/updater/WeatherConditionUpdaterView.java
@@ -22,74 +22,105 @@ public class WeatherConditionUpdaterView implements Observer, IWeatherConditionU
 
     IWeatherConditionUpdaterController controller;
 
-
     public void setContentView(Poi poiToUpdate, View view, Context context){
+        // Setup information part
+        setupInformationPart(poiToUpdate, view, context);
+
+        // Setup buttons
+        setupButtons(view);
+
+        // Edition part
+        setupEditionPart(poiToUpdate, view, context);
+
+        // Deletion part
+        setupDeletionPart(poiToUpdate, view, context);
+    }
+
+
+    @Override
+    public void setController(IWeatherConditionUpdaterController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
+
+    private void setDateTimeField(Poi poiToUpdate, TextView textView, Context context, DateFormat inputFormat, DateFormat outputFormat, String stringResource) {
+        int stringId = context.getResources().getIdentifier(stringResource, "string", context.getPackageName());
+        try {
+            Date date = inputFormat.parse(poiToUpdate.getCreatedAt());
+            String formattedDateTime = outputFormat.format(date);
+            textView.setText(context.getString(stringId, formattedDateTime));
+        } catch (ParseException e) {
+            textView.setText(context.getString(stringId, poiToUpdate.getCreatedAt()));
+        }
+    }
+
+    private void setupInformationPart(Poi poiToUpdate, View view, Context context){
+        // Title
         TextView title = view.findViewById(R.id.title);
         int stringTitle = context.getResources().getIdentifier("weather_condition_updater_title", "string", context.getPackageName());
         title.setText(context.getString(stringTitle, poiToUpdate.isFinished() ? " - Terminé" : " - En cours"));
 
+        // Coordinates
         TextView coordinates = view.findViewById(R.id.coordinates);
         int stringCoordinates = context.getResources().getIdentifier("weather_condition_updater_coordinates", "string", context.getPackageName());
         coordinates.setText(context.getString(stringCoordinates, poiToUpdate.getLatitude(), poiToUpdate.getLongitude()));
 
+        // Weather condition
         TextView weatherCondition = view.findViewById(R.id.condition);
         int stringCurrentWeatherCondition = context.getResources().getIdentifier("weather_condition_updater_current_condition", "string", context.getPackageName());
         int stringWeatherCondition = context.getResources().getIdentifier("weather_" + poiToUpdate.getWeatherCondition().toString().toLowerCase(Locale.ROOT), "string", context.getPackageName());
         weatherCondition.setText(context.getString(stringCurrentWeatherCondition, context.getString(stringWeatherCondition)));
 
+        // Perimeter
         TextView perimeter = view.findViewById(R.id.perimeter);
         int stringPerimeter = context.getResources().getIdentifier("weather_condition_updater_perimeter", "string", context.getPackageName());
         perimeter.setText(context.getString(stringPerimeter, (int) poiToUpdate.getPerimeter()));
 
+        // Setup date formatter
         DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.FRANCE);
         inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         DateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy HH'h'mm", Locale.FRANCE);
         outputFormat.setTimeZone(TimeZone.getDefault());
 
-        TextView createdAt = view.findViewById(R.id.created_at);
-        int stringCreatedAt = context.getResources().getIdentifier("weather_condition_updater_created_at", "string", context.getPackageName());
-        try {
-            Date dateCreatedAt = inputFormat.parse(poiToUpdate.getCreatedAt());
-            createdAt.setText(context.getString(stringCreatedAt, outputFormat.format(dateCreatedAt)));
-        } catch (ParseException e) {
-            createdAt.setText(context.getString(stringCreatedAt, poiToUpdate.getCreatedAt()));
-        }
+        // Created at - date field
+        setDateTimeField(poiToUpdate, view.findViewById(R.id.created_at), context, inputFormat, outputFormat, "weather_condition_updater_created_at");
 
-        TextView lastUpdate = view.findViewById(R.id.last_update);
-        int stringLastUpdate = context.getResources().getIdentifier("weather_condition_updater_last_update", "string", context.getPackageName());
-        try {
-            Date dateUpdatedAt = inputFormat.parse(poiToUpdate.getUpdatedAt());
-            lastUpdate.setText(context.getString(stringLastUpdate, outputFormat.format(dateUpdatedAt)));
-        } catch (ParseException e) {
-            lastUpdate.setText(context.getString(stringLastUpdate, poiToUpdate.getUpdatedAt()));
-        }
+        // Updated at - date field
+        setDateTimeField(poiToUpdate, view.findViewById(R.id.last_update), context, inputFormat, outputFormat, "weather_condition_updater_last_update");
 
+        // Added by
         TextView addedBy = view.findViewById(R.id.added_by);
         int stringAddedBy = context.getResources().getIdentifier("weather_condition_updater_added_by", "string", context.getPackageName());
         addedBy.setText(context.getString(stringAddedBy, poiToUpdate.getCreatorFullname()));
+    }
 
+    private void setupButtons(View view){
+        // Close icon
+        view.findViewById(R.id.close_button).setOnClickListener(v -> controller.userActionCloseFragment());
 
-        TextView closeButton = view.findViewById(R.id.close_button);
-        closeButton.setOnClickListener(v -> controller.userActionCloseFragment());
+        // Close button
+        view.findViewById(R.id.close).setOnClickListener(v -> controller.userActionCloseFragment());
 
-        Button close = view.findViewById(R.id.close);
-        close.setOnClickListener(v -> controller.userActionCloseFragment());
+        // Finished button
+        view.findViewById(R.id.finished).setOnClickListener(v -> controller.userActionDeletePoi());
+    }
 
-        Button finished = view.findViewById(R.id.finished);
-        finished.setOnClickListener(v -> controller.userActionDeletePoi());
-
-        // Delete part
+    private void setupDeletionPart(Poi poiToUpdate, View view, Context context){
         TextView informationText = view.findViewById(R.id.click_if_is_finished);
         if(poiToUpdate.isFinished()){
             // Change information text
             int stringFinished = context.getResources().getIdentifier("weather_condition_updater_button_information_already_finished", "string", context.getPackageName());
             informationText.setText(context.getString(stringFinished));
-
             // Set "delete" button to red background
-            finished.setBackgroundColor(context.getResources().getColor(R.color.red));
+            view.findViewById(R.id.finished).setBackgroundColor(context.getResources().getColor(R.color.red));
         }
+    }
 
-        // Edition part
+    private void setupEditionPart(Poi poiToUpdate, View view, Context context){
         if(poiToUpdate.isFinished()){
             // Hide edition_layout if the poi is finished
             view.findViewById(R.id.edition_layout).setVisibility(View.GONE);
@@ -116,15 +147,5 @@ public class WeatherConditionUpdaterView implements Observer, IWeatherConditionU
                 public void onStopTrackingTouch(SeekBar seekBar) {}
             });
         }
-    }
-
-    @Override
-    public void setController(IWeatherConditionUpdaterController controller) {
-        this.controller = controller;
-    }
-
-    @Override
-    public void update(Observable observable, Object o) {
-
     }
 }
